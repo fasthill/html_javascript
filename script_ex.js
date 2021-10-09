de672e5a575af5a80a2b18b1325af6653d1c19f1
fasthill <kangeun@naver.com>
2021-10-09 22:30:33 +0900

    location

---
+++ script_ex.js
@@ -0,0 +1,11 @@
+let url = new URL('https://developer.mozilla.org:8080/en-US/search?q=URL#search-results-close-container');
+
+console.log(url.toString(), url.href);
+console.log(url);
+console.log(url.protocol);
+console.log(url.host);
+console.log(url.port);
+console.log(url.pathname);
+console.log(url.search);
+console.log(url.hash);
+
